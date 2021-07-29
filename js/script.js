7e529f9db23480d7df7afcb5d70ece2d5f73792d
Pedro Lima <pedrozin1234@gmail.com>
2021-07-29 02:39:42 -0300

    Validations in js, and layou att

---
+++ js/script.js
@@ -0,0 +1,46 @@
+let focus = document.querySelectorAll('.nav-link');
+focus.forEach(navitem => {
+    navitem.addEventListener('click', function(){
+        focus.forEach(focusMenu => focusMenu.classList.remove('active'));
+        this.classList.add('active');
+    });
+}); 
+
+
+
+
+/* MASKS AND VALIDATES*/
+
+const masks = {
+    
+    tel(value){
+        return value
+        .replace(/\D/g, '')
+        .replace(/(\d{2})(\d)/, '($1) $2')
+        .replace(/(\d{5})(\d)/, '$1-$2')
+    },
+
+}
+
+document.querySelectorAll('input').forEach(($input) => {
+    const receipt = $input.dataset.js
+
+    $input.addEventListener('input', (e) => {
+        e.target.value = masks[receipt](e.target.value);
+    });
+});
+
+
+/* Validation Name */
+
+function validation () {
+    var name = document.getElementById('name-input').value
+
+    if ( name == ''){
+        alert('Você precisa digitar um nome')
+    }
+
+    else if (!!name.match(/[A-Z][a-z]* [A-Z][a-z]*/) == 0) {
+        alert('Digite um nome completo, lembre-se das iniciais maiusculas')
+    }
+}
